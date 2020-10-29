e4790a9c0ce655c32c4b8541172ed9e4a8897bae
Ubuntu <ubuntu@ip-10-0-1-136.me-south-1.compute.internal>
2020-10-29 21:11:22 +0000

    updated

--- e2e-js-test/catalogue_test.js
+++ e2e-js-test/catalogue_test.js
@@ -2,23 +2,41 @@
   'use strict';
   require("./config");
 
-  casper.test.begin("User interacts with the catalogue", 2, function(test) {
+  casper.test.begin("User interacts with the cart", 1, function(test) {
+    // initial load and login
     casper.start("http://front-end/", function() {
-      this.waitForText("Catalogue", function() {
-        this.clickLabel("Catalogue");
+      this.clickLabel("Login");
+      this.fill("#login-modal form", {
+        "username": "Eve_Berger",
+        "password": "eve"
+      }, true);
+      this.click("#login-modal form button.btn.btn-primary");
+      this.waitForText("Logged in", function() {
       }, function() {
-        test.error("page not yet initialised");
+        test.fail("login failed");
       }, 3000);
     });
 
+    // access the catalogue
     casper.then(function() {
-      this.waitForSelector("#products", function() {
-        test.pass("user is taken to the catalogue");
-        test.assertElementCount("#products div.product", 6, "user is presented with 6 products by default");
+      this.clickLabel("Catalogue");
+    });
+
+    // Add some items to the cart and verify
+    casper.then(function() {
+      this.waitForText("Add to cart", function() {
+        this.clickLabel("Add to cart");
       }, function() {
-        test.fail("catalogue page did not load")
+        test.fail("Catalogue items did not show up");
+      }, 3000)
+
+      this.waitForText("1 item(s) in cart", function() {
+        test.pass("cart gets updated with user selection");
+      }, function() {
+        test.fail("cart was not updated");
       }, 3000);
     });
+
 
     casper.run(function() {
       test.done();
