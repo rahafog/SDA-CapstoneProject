4fb34930d231007a76ccfd0f6e6726b9a0f7086d
Ubuntu <ubuntu@ip-10-0-1-247.me-south-1.compute.internal>
2020-10-30 12:59:05 +0000

    update logs file

--- e2e-js-test/runner.sh
+++ e2e-js-test/runner.sh
@@ -6,12 +6,12 @@ files=$testfile
 
 if [ -z $testfile ]
 then
-  files=$(ls /e2e-test/*_test.js)
+  files=$(ls /e2e-js-test/*_test.js)
 fi
 
 for test in $files
 do
-  #node /e2e-test/test_helper.js
+  #node test/e2e-js-test/test_helper.js
   casperjs test $test
   ret=$?
   if [ ! $ret == "0" ]; then code=1; fi
